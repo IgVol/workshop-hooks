9285c5c04a9e286e53cd8f478acc2a782a93504f
IgVol <igvol44@gmail.com>
2020-08-27 14:51:22 +0300

    Set up app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-const states = []
-let calls = -1 // !!
-
-function useState(defaultValue) {
-  const callId = ++calls // !!
-  if (states[callId]) {
-    return states[callId]
-  }
-  const setValue = newValue => {
-    states[callId][0] = newValue;
-    renderWithCustomHooks();
-  }
-  const tuple = [defaultValue, setValue]
-  states[callId] = tuple // !!
-  return tuple
-}
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,15 +13,9 @@ function App() {
   );
 }
 
-
-const renderWithCustomHooks = () => {
-  calls = -1;
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-renderWithCustomHooks()
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
